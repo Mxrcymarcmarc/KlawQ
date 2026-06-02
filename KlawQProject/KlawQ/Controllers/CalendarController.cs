b746a6fd62fed9cb00e32976b2bf1dbc15dc3b4d
John Paul Levi Magtibay <johnpaullevipmagtibay@iskolarngbayan.pup.edu.ph>
2026-06-02 22:08:23 +0800

    still work in progress

---
+++ KlawQProject/KlawQ/Controllers/CalendarController.cs
@@ -0,0 +1,49 @@
+﻿using KlawQ.Models;
+using KlawQ.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace KlawQ.Controllers
+{
+    public class CalendarDayStatus
+    {
+        public string DateString { get; set; } = string.Empty;
+        public bool IsAvailable { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CalendarController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public calendarcontroller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("current-view-status")]
+        public async Task<IActionResult> GetCurrentCalendarView()
+        {
+            DateTime today = DateTime.Now;
+            int currentYear = today.Year;
+            int currentMonth = today.Month;
+
+            var currentMonthDays = await GetDaysStatusForMonth(currentYear, currentMonth);
+
+            int availableDaysLeft = currentMonthDays.Count(d => d.IsAvailable && DateTime.Parse(d.DateString) >= today);
+
+            const int Almost_full_treshold = 5;
+
+            var finalResponse = new List<CalendarDayStatus>();
+            finalResponse.AddRange(currentMonthDays);
+
+        }
+    }
+}
--- KlawQProject/KlawQ/Data/ApplicationDbContext.cs
+++ KlawQProject/KlawQ/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using KlawQ.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KlawQ.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<Scheduler> Schedules { get; set; }
     }
 }
