01dc9b1241dd879d97bfb28ac2aaca6198422021
John Paul Levi Magtibay <johnpaullevipmagtibay@iskolarngbayan.pup.edu.ph>
2026-06-05 17:28:52 +0800

    fixed ApplicatioinDbContext where merge was not successfull

--- KlawQProject/KlawQ/Data/ApplicationDbContext.cs
+++ KlawQProject/KlawQ/Data/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
+using KlawQ.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KlawQ.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<Scheduler> Schedulers { get; set; }
     }
+}
