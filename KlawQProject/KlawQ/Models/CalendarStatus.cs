43ad942c5d04a133b6cbfc19b5cafde8137a62c3
John Paul Levi Magtibay <johnpaullevipmagtibay@iskolarngbayan.pup.edu.ph>
2026-06-07 22:57:01 +0800

    Moved the CalendarDayStatus class to Models folder. Fixed the status where it would make the same day available to book, changed it so that users cannot book the same day.

---
+++ KlawQProject/KlawQ/Models/CalendarStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace KlawQ.Models
+{
+    public class CalendarDayStatus
+    {
+        public string DateString { get; set; } = string.Empty;
+        public bool IsAvailable { get; set; }
+
+    }
+}
